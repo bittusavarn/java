917206826822e1b23613b0b80d0f13bc1b8d37c4
Nilamber Kumar <bittusavarn@gmail.com>
2014-02-01 20:33:00 +0530

    just a commit

--- SoapProject/src/com/soap/Main.java
+++ SoapProject/src/com/soap/Main.java
@@ -1,7 +1,11 @@
 package com.soap;
 
 import javax.xml.ws.Endpoint;
-
+/**
+ * 
+ * @author Nilamber Kumar
+ *
+ */
 public class Main {
 	public static void main(String[] args) {
 		   Endpoint.publish("http://localhost:9999/ws/hello", new SoapTestImpl());
